55939488790279fe853294d4155bcb3a8719c31e
Ivan Reynoso <IvanReynoso@Ivans-MacBook-Pro.local>
2018-04-25 20:17:13 -0700

    adds initial project files

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("main is running")
+
+$(document).ready(function() {
+    $.getJSON( "./data.json", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<tr><td>" + val.name + "</td><td>" + val.description + "</td><td><a href='https://www.google.com/maps?q=" + val.location + "'>link</a></td></tr>" );
+        });
+        
+        $('table').append( items );
+    });
+});
